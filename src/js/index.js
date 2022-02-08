0382adb25e98998a30bb1218fd16203aa3ed9418
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-08 12:00:28 -0500

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ const Stocks = require('stocks.js');
 const stocks = new Stocks('FWEGZI6T908UAAMF');
 
 for (let i = 0; i <= 365; i++) {
-  setInterval( async test => {
+  setInterval(async test => {
     let result = await stocks.timeSeries({
       symbol: 'GTLB',
       interval: '1day',
