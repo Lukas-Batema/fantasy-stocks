6f3f08a74511fa340f1bcdeb33a7deb718342a42
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-08 11:30:34 -0500

    fix?

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ const Stocks = require('stocks.js');
 
 const stocks = new Stocks('FWEGZI6T908UAAMF');
 
-let result = await stocks.timeSeries({
+let result = stocks.timeSeries({
   symbol: 'GTLB',
   interval: '1min',
   amount: 10
 });
+
+console.log(result);
