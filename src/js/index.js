888f054ff3dbb364b957697ae6c0ae4a3f9a58c4
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-08 11:54:04 -0500

    test

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@ const Stocks = require('stocks.js');
 
 const stocks = new Stocks('FWEGZI6T908UAAMF');
 
-async function test() {
-  let result = await stocks.timeSeries({
-    symbol: 'GTLB',
-    interval: '1min',
-    amount: 10
-  });
+for (let i = 0; i <= 365; i++) {
+  setInterval( async test => {
+    let result = await stocks.timeSeries({
+      symbol: 'GTLB',
+      interval: '1day',
+      amount: 365
+    });
 
-  console.log(result);
+    console.log(result);
+  }, 1000 * 60 * 60 * 24);
 }
-
-test();
