243ee51af2537929ba75bf02bb91b1a86a0df58e
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-09 09:12:50 -0500

    test

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM nginx:alpine
+COPY docs /usr/share/nginx/html
+
+# Make Directorty of `static` and COPY it
+# RUN mkdir /usr/share/nginx/html/static
+# COPY static /usr/share/nginx/html/static
+
+# Make Directorty of `src` and COPY it
+RUN mkdir /usr/share/nginx/html/src
+COPY src /usr/share/nginx/html/src
--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,26 @@ const Stocks = require('stocks.js');
 
 const stocks = new Stocks('FWEGZI6T908UAAMF');
 
+// Get the stats once per day, for one year (need to figure out how to have this go indefintly, without having a mass memory leak)
 for (let i = 1; i <= 365; i++) {
-  setInterval(async test => {
-    let result = await stocks.timeSeries({
+  // Get the stats for a number of different stocks, once per day.
+  setInterval(async dailyStats => {
+    let gtlbDailyResult = await stocks.timeSeries({
       symbol: 'GTLB',
       interval: 'daily',
       amount: 1
     });
 
-    console.log(result);
+    // Log the GTLB daily stock stats in console for now
+    console.log(gtlbDailyResult);
+
+    let applDailyResult = await stocks.timeSeries({
+      symbol: 'APPL',
+      interval: 'daily',
+      amount: 1
+    });
+
+    // Log the APPL daily stock stats in console for now
+    console.log(applDailyResult);
   }, 1000 * 60 * 60 * 24);
 }
