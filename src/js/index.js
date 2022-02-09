7b732374bb1639244e22e5f952d1604b24a0a08d
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-09 13:43:40 -0500

    fix?

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,12 @@ const Stocks = require('stocks.js');
 
 const stocks = new Stocks('FWEGZI6T908UAAMF');
 
-for (let i = 1; i <= 365; i++) {
-  setInterval(async test => {
-    let result = await stocks.timeSeries({
-      symbol: 'GTLB',
-      interval: 'daily',
-      amount: 1
-    });
+setInterval(async test => {
+  let result = await stocks.timeSeries({
+    symbol: 'GTLB',
+    interval: 'daily',
+    amount: 1
+  });
 
-    console.log(result);
-  }, 1000 * 60 * 60 * 24);
-}
+  console.log(result);
+}, 1000 * 60 * 60 * 24);
