febe90a6bdbb89cfe36e739024804d6a3ea32ef3
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-16 12:39:34 -0500

    test

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ setInterval(async () => {
     amount: 1
   });
 
-  let gtlbFormatResult = await Object.entries(gtlbResult).toString().substring(2, 7);
+  let gtlbFormatResult = Object.keys(gtlbResult).toString().substring(2, 7);
 
   console.log('GTLB (GitLab):');
   console.log(gtlbFormatResult);
