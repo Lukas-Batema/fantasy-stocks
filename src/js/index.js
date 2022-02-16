cbef6675a901b252dcafcafcec5823e447d5b062
Lukas-Batema <lukasbatema@gmail.com>
2022-02-16 15:52:33 -0500

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,16 @@
-const Stocks = require('stocks.js');
-const stocks = new Stocks('FWEGZI6T908UAAMF');
+const request = require('request');
+let url = 'https://www.alphavantage.co/query?function=function=GLOBAL_QUOTES&symbol=GTLB&apikey=FWEGZI6T908UAAMF';
 
-setInterval(async () => {
-  let gtlbResult = await stocks.timeSeries({
-    symbol: 'GTLB',
-    interval: '5min',
-    amount: 1
-  });
-
-  console.log('GTLB (GitLab):');
-  console.log(gtlbResult);
-  console.log("----------");
-}, 1000 * 1 * 60 * 5);
+request.get({
+  url: url,
+  json: true,
+  headers: { 'User-Agent': 'request' }
+}, (err, res, data) => {
+  if (err) {
+    console.log('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.log('Status:', res.statusCode);
+  } else {
+    console.log(data);
+  }
+});
