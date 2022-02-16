699e74ad6a1fd26355a785d35da4e88564b107fe
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-16 11:40:02 -0500

    ...

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,13 @@
 const Stocks = require('stocks.js');
 const stocks = new Stocks('FWEGZI6T908UAAMF');
 
-setInterval(async UwU => {
+setInterval(async test => {
   let gtlbResult = await stocks.timeSeries({
     symbol: 'GTLB',
     interval: '5min',
     amount: 1
   });
 
-  console.log(`
-    **GTLB (GitLab)**:
-    \n
-    ${gtlbResult}
-  `);
+  console.log('GTLB (GitLab):');
+  console.log(gtlbResult);
 }, 1000 * 1 * 60 * 5);
