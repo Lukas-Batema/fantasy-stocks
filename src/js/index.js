febd6e5c35b552e2c9f6bc1f7c2e3703d32a74ae
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-17 07:33:49 -0500

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 const request = require('request');
 let url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GTLB&apikey=FWEGZI6T908UAAMF';
 
-request.get({
-  url: url,
-  json: false,
-  headers: { 'User-Agent': 'request' }
-}, (err, res, data) => {
-  if (err) {
-    console.log('Error:', err);
-  } else if (res.statusCode !== 200) {
-    console.log('Status:', res.statusCode);
-  } else {
-    console.log(data);
-  }
-});
+setInterval(() => {
+  request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      console.log(data);
+    }
+  });
+}, 1000 * 1 * 60 * 5);
