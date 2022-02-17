ab9dfbb1e732c20784eec06cfa7ea5b10bee15e6
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-17 11:34:19 -0500

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,33 @@
 const request = require('request');
-let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GTLB&apikey=${process.env.API_KEY}`;
 
-request.get({
-  url: url,
-  json: true,
-  headers: { 'User-Agent': 'request' }
-}, (err, res, data) => {
-  if (err) {
-    console.log('Error:', err);
-  } else if (res.statusCode !== 200) {
-    console.log('Status:', res.statusCode);
-  } else {
-    console.log(data);
+setInterval(() => {
+  let randSymbol = Math.round(Math.random() * 5);
+
+  let symbol = '^DJI';
+
+  if (randSymbol === 0) {
+    symbol = 'GTLB';
+  } else if (randSymbol === 1) {
+    symbol = 'LAC';
+  } else if (randSymbol === 2) {
+    symbol = 'MSFT';
+  } else if (randSymbol === 3) {
+    symbol = 'APPL';
   }
-});
+
+  let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`;
+
+  request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      console.log(`${symbol}:`, data);
+    }
+  });
+}, 1000 * 60 * 60 * 24);
