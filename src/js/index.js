90a47406b4f554f5701c2037269dcbfa89771f9b
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-17 11:09:39 -0500

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,16 @@
 const request = require('request');
-let url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GTLB&apikey=FWEGZI6T908UAAMF';
+let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GTLB&apikey=${process.env.API_KEY}`;
 
-setInterval(() => {
-  request.get({
-    url: url,
-    json: true,
-    headers: { 'User-Agent': 'request' }
-  }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      console.log(data);
-    }
-  });
-}, 1000 * 1 * 60 * 5);
+request.get({
+  url: url,
+  json: true,
+  headers: { 'User-Agent': 'request' }
+}, (err, res, data) => {
+  if (err) {
+    console.log('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.log('Status:', res.statusCode);
+  } else {
+    console.log(data);
+  }
+});
