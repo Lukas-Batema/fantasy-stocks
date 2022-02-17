7c9ed0bcbaf8c2d063daa55c337c599360adde53
BatemaDevelopment <lukasbatema@gmail.com>
2022-02-17 11:56:07 -0500

    test

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,21 @@ setInterval(() => {
   let randSymbol = Math.round(Math.random() * 5);
 
   let symbol = '^DJI';
+  let url = 'https://www.alphavantage.co/'
 
   if (randSymbol === 0) {
     symbol = 'GTLB';
+    url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`;
   } else if (randSymbol === 1) {
     symbol = 'LAC';
+    url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`;
   } else if (randSymbol === 2) {
     symbol = 'MSFT';
+    url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`;
   } else if (randSymbol === 3) {
     symbol = 'APPL';
+    url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`;
   }
-
-  let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.API_KEY}`;
 
   request.get({
     url: url,
